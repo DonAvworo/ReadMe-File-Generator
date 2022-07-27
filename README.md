6e94774f9c3701426afde990e04dfb659f9ed18d
Don Avworo <101209035+DonAvworo@users.noreply.github.com>
2022-07-27 19:31:03 +0100

    long day coding

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# week-9-homework
+ 
+  #whatever
+  ## License:
+  
+  [![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Description:
+  whatever
+  ## Table of contents
+    * [Title](#title)
+    * [License](#license)
+    * [Description:](#description)
+    * [Table of contents](#table-of-contents)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Technologies](#technologies)
+    * [Frameworks](#frameworks)
+    * [Dependencies](#dependencies)
+    * [Author](#author)
+    * [Contributing](#contributionguidelines)
+    * [Acknowledgements](#acknowledgements)
+    * [Test](#ttestInstructions)
+    * [Questions](#questions)
+    * [GitHub-User](#githubUsername)
+    * [GitHub-Url](#githuburl)
+    * [Email](#email)
+  
+  ## Installation:
+  ---
+  whatever
+
+  ---
+  ## Usage:
+  whatever
+  ## Technologies:
+  js
+  ## Frameworks:
+  node environment
+  ## Dependencies:
+  npm i
+  ## Author:
+  whjatever
+  ## Contributing:
+  call me
+  ## Acknowledgements:
+  a b c
+  ## Test: 
+  test
+  ## Questions:
+  ghdshda
+  ## GitHubUsername:
+  undefined
+  ## GitHubUrl:
+  undefined
+  ## Email:
+  
+    
+
+  Embed images using the following syntax
+
+  -1 Markup Syntax
+  ![]()
+  ![image descriptiom](image location)
+  ![](homework/week-3-homework/assets/img/Web%20KeyGen.png)
+
+  -2 Html Syntax
+  <img src="" alt="">
+  <img src="imageName.format" alt="image description">
+
--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -2,27 +2,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'MIT' || 'Apache' || 'GPL' || 'BSD' || 'None') {  //if the license is MIT, return the following string with url
+  //if the license is MIT, return the following string with url
+  if (license === 'MIT' || 'Apache' || 'GPL' || 'BSD' || 'None') {  
     return '[![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
   }
-  else if (license === 'ISC') { //if the license is ISC, return the following string with url
+  //if the license is ISC, return the following string with url
+  else if (license === 'ISC') { 
     return '[![ISC License](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
   }
   else {
-    return ''; // if no license is found, return an empty string
+    // if no license is found, return an empty string
+    return ''; 
   }
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(license) { 
   if (license === license) {                              //if the license is equal to the license, return the license link
     return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/${license})
-        `;
+    `;
   }
 
   else {
-    return ''; // if no license is found, return an empty string
+    // if no license is found, return an empty string
+    return ''; 
   }
 }
 
@@ -33,19 +37,19 @@ function renderLicenseSection(license) {
   //if the license is MIT, return the following string
   if (license === 'MIT') { 
     return `
-
-        [![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-        `;
+  [![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  `;
   }
   //if the license is Apache, return the following string
   else if (license === 'Apache') { 
     return `                      
-        ## License 
-        [![Apache License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-        `;
+    
+  [![Apache License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  `;
   }
   else {
-    return ''; // if no license is found, return an empty string
+    // if no license is found, return an empty string
+    return ''; 
   }
 };
 
@@ -53,52 +57,77 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
-  //mind the backticks (`) around the data.title and data.description variables TAKE CARE not to remove them
-  console.log(data)
+  // console.log(data) 
 
+  // call the function to render the license section
   const license = renderLicenseSection(data.license);
   
-
-  return `  
-    
-    # ${data.title}
-    ## Description
-    ${data.description}
-    ## Table of contents
-    ${data.tableOfContents}
+  return ` 
+  # Title: 
+  ${data.title}
+  ## License:
+  ${license}
+  ## Description:
+  ${data.description}
+  ## Table of contents
     * [Title](#title)
-    
-    * [General info](#general-info)
+    * [License](#license)
+    * [Description:](#description)
+    * [Table of contents](#table-of-contents)
+    * [Installation](#installation)
+    * [Usage](#usage)
     * [Technologies](#technologies)
-    * [Setup](#setup)
+    * [Frameworks](#frameworks)
+    * [Dependencies](#dependencies)
     * [Author](#author)
+    * [Contributing](#contributionguidelines)
+    * [Acknowledgements](#acknowledgements)
+    * [Test](#ttestInstructions)
+    * [Questions](#questions)
+    * [GitHub-User](#githubUsername)
+    * [GitHub-Url](#githuburl)
+    * [Email](#email)
+  ${data.tableOfContents}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## Technologies:
+  ${data.technologies}
+  ## Frameworks:
+  ${data.frameworks}
+  ## Dependencies:
+  ${data.dependencies}
+  ## Author:
+  ${data.author}
+  ## Contributing:
+  ${data.contributionGuidelines}
+  ## Acknowledgements:
+  ${data.acknowledgements}
+  ## Test: 
+  ${data.testInstructions}
+  ## Questions:
+  ${data.questions}
+  ## GitHubUsername:
+  ${data.githubusername}
+  ## GitHubUrl:
+  ${data.githuburl}
+  ## Email:
+  ${data.email}
+    
 
+  Embed images using the following syntax
 
-    ## General info
-    This project is simple Lorem ipsum dolor generator.
-	
-    ## Technologies
-    Project is created with:
-    * Lorem version: 12.3
-    * Ipsum version: 2.33
-    * Ament library version: 999
-	
-    ## Setup
-    To run this project, install it locally using npm:
+  -1 Markup Syntax
+  ![]()
+  ![image descriptiom](image location)
+  ![](homework/week-3-homework/assets/img/Web%20KeyGen.png)
 
-    ${license}
-    Embedding images using
-
-    1) Markup Syntax
-    ![]()
-    ![image descriptiom](image location)
-    ![](homework/week-3-homework/assets/img/Web%20KeyGen.png)
-
-    2)HTML SAYNTAX
-    <img src="" alt="">
-    <img src="imageName.format" alt="image description">
+  -2 Html Syntax
+  <img src="" alt="">
+  <img src="imageName.format" alt="image description">
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown; // Export the generateMarkdown function for use in the index.js/controller
