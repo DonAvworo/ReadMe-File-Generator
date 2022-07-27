8ac378e0cfcb687bfd2977f2a729dca08a732d3f
Don Avworo <101209035+DonAvworo@users.noreply.github.com>
2022-07-27 19:30:11 +0100

    long day coding

--- index.js
+++ index.js
@@ -7,16 +7,34 @@ const init = () => {
     /*create an array of question prompts, using the created variable (const questions = require('inquirer');) that the user will answer 
     to generate the README file and pass the data into the generateMarkdown function*/
     inquirer.prompt([
+
+        //ask the user to enter a table of contents
+        {
+            type: 'input',
+            name: 'tableOfContents',
+            message: 'A table of contents has been automatically generated for You. Please press enter to continue.',
+            validate: function (input) { 
+                if (input.length > 1) {
+                    return 'Please delete (backspace bttton) the characters typed in and press enter to continue.';
+                }
+                //if the input is valid, or no input, return true
+                else { 
+                    return true;
+                }
+            }
+        },
+
         // ask the user for the the project title
         {
             type: 'input',
             name: 'title',
             message: 'What do you want to call this project? (e.g. "My New Project")',
             validate: function (input) {
-                if (input.length < 1 || input.length > 100 || input.includes(' ')) {
-                    return 'Please enter a title for your project. It must be between 1 and 100 characters and cannot contain spaces.';
+                if (input.length < 1 || input.length > 100 || input === ' ') {
+                    return 'Please enter a title for your project. It must be between 1 and 100 characters.';
                 }
-                else { //if the input is valid, return true
+                //if the input is valid, return true
+                else { 
                     return true;
                 }
             }
@@ -38,17 +56,76 @@ const init = () => {
             }
         },
 
-        //ask the user to enter a table of contents
-        {
-            type: 'input',
-            name: 'tableOfContents',
-            message: 'Please enter a table of contents for your project. (e.g. "Installation, Usage, License, Contributing, Tests, Questions")',
-            validate: function (input) { //if the input is valid, return true
-                if (input.length < 1 || input.length > 10000) {
-                    return 'Please enter a table of contents for your project. It must be between 1 and 10000 characters.';
+        //ask the user to enter the installation instructions
+        {   type: 'input',
+            name: 'installation',
+            message: 'Please enter the installation instructions for your project.',
+            validate: function (input) { 
+                if (input.length < 1 || input.length > 1000) {
+                    return 'Please enter the installation instructions for your project. It must be between 1 and 1000 characters.';
                 }
-                //if the input is valid, return true
-                else { 
+                //if the input is valid, or no input, return true
+                else {
+                    return true;
+                }
+            } 
+        },
+
+        //ask the user to enter the usage instructions
+        {   type: 'input',
+            name: 'usage',
+            message: 'Please enter the usage instructions for your project.',
+            validate: function (input) {
+                if (input.length < 1 || input.length > 1000) {
+                    return 'Please enter the usage instructions for your project. It must be between 1 and 1000 characters.';
+                }
+                //if the input is valid, or no input, return true
+                else {
+                    return true;
+                }
+            }
+        },
+
+        //ask the user to enter the technologies used
+        {   type: 'input',
+            name: 'technologies',
+            message: 'Please enter the technologies used for your project.',
+            validate: function (input) {
+                if (input.length < 1 || input.length > 1000) {
+                    return 'Please enter the technologies used for your project. It must be between 1 and 1000 characters.';
+                }
+                //if the input is valid, or no input, return true
+                else {
+                    return true;
+                }
+            }
+        },
+
+        //ask the user to enter the frameworks used
+        {   type: 'input',
+            name: 'frameworks',
+            message: 'Please enter the frameworks used for your project.',
+            validate: function (input) {
+                if (input.length < 1 || input.length > 1000) {
+                    return 'Please enter the frameworks used for your project. It must be between 1 and 1000 characters.';
+                }
+                //if the input is valid, or no input, return true
+                else {
+                    return true;
+                }
+            }
+        },
+
+        //ask the user to enter the dependencies
+        {   type: 'input',
+            name: 'dependencies',
+            message: 'Please enter the dependencies for your project.',
+            validate: function (input) {
+                if (input.length < 1 || input.length > 1000) {
+                    return 'Please enter the dependencies for your project.eg. "npm install inquirer".';
+                }
+                //if the input is valid, or no input, return true
+                else {
                     return true;
                 }
             }
@@ -60,6 +137,21 @@ const init = () => {
             name: 'license',
             message: 'Please select a license for your project.',
             choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+        },
+
+        //ask the user to enter the name of the author
+        {   type: 'input',
+            name: 'author',
+            message: 'Please enter the name of the author of your project.',
+            validate: function (input) {    
+                if (input.length < 1 || input.length > 100) {
+                    return 'Please enter the name of the author of your project. It must be between 1 and 100 characters.';
+                }
+                //if the input is valid, or no input, return true
+                else {
+                    return true;
+                }
+            }
         },
 
         //ask the user to enter a contribution guidelines
@@ -81,41 +173,22 @@ const init = () => {
         //ask the user to enter the names of creators and contributors
         {
             type: 'input',
-            name: 'test',
-            message: 'Please enter the names of creators and contributors. (e.g. "The following people have contributed to this project: Hope Pope, Moe Doe, Selina Williams, Craig Golliath.")',
-            validate: function (input) {
-                if (input.length < 1 || input.length > 10000) {
-                    return 'Maybe later? type in, Yep or Nope to continue.'; //for user interaction, otherwise pointless ;)
-                }
-                //if the input is valid, return true
-                else { 
-                    return true;
-                }
-            }
+            name: 'acknowledgements',
+            message: 'Please enter the names of contributors of your project.',
         },
-
+        
         //ask the user to enter a test instructions
         {
             type: 'input',
             name: 'testInstructions',
             message: 'Please enter a test instructions for your project. (e.g. "To run tests, run the tests command.")',
-            validate: function (input) {
-                if (input.length < 1 || input.length > 10000) {
-                    return 'Maybe later? type in, Yep or Nope to continue.'; //for user interaction, otherwise pointless (trick is, user still meets the criteria of two or more characters);)
-                }
-                //if the input is valid, return true
-                else { 
-                    return true;
-                }
-            }
         },
 
-        //ask the user to enter a the techonlogy used in the project
+        // ask the user to enter questions for the project
         {
-            type: 'list',
-            name: 'technologyUsed',
-            message: 'Please select the technology used in your project.',
-            choices: ['HTML', 'CSS', 'JS', 'MySQL', 'MongoDB', 'React', 'Python', 'PHP', 'Some of the above plus other technologies', 'None']
+            type: 'input',
+            name: 'questions',
+            message: 'Please enter questions for your project. (e.g. "How do I use this project?")'
         },
 
         //ask the user to enter a the github username of the user who created the project
@@ -123,15 +196,6 @@ const init = () => {
             type: 'input',
             name: 'githubUsername',
             message: 'Please enter the github username of the user who created the project.',
-            validate: function (input) {
-                if (input.length < 1 || input.length > 100 || input.includes(' ')) {
-                    return 'Please enter the github username of the user who created the project. It must be between 1 and 100 characters and cannot contain spaces.';
-                }
-                //if the input is valid, return true
-                else { 
-                    return true;
-                }
-            }
         },
 
         //ask the user to enter a the github url of the user who created the project
@@ -139,15 +203,6 @@ const init = () => {
             type: 'input',
             name: 'githubUrl',
             message: 'Please enter the github url of the user who created the project.',
-            validate: function (input) {
-                if (input.length < 1 || input.length > 100 || input.includes(' ')) {
-                    return 'Please enter the github url of the user who created the project. It must be between 1 and 100 characters and cannot contain spaces.';
-                }
-                //if the input is valid, return true
-                else { 
-                    return true;
-                }
-            }
         },
 
         //ask the user to enter a the email of the user who created the project
@@ -155,22 +210,14 @@ const init = () => {
             type: 'input',
             name: 'email',
             message: 'Please enter the email of the user who created the project.',
-            validate: function (input) { 
-                if (input.length < 1 || input.length > 100 || input.includes(' ')) {
-                    return 'Maybe later? type in, Yep or Nope to continue.';
-                }
-                //if the input is valid, return true
-                else { 
-                    return true;
-                }
-            }
+    
         }
     ]).then(answers => {
 
-        const content = generateMarkdown(answers); // 
+        const content = generateMarkdown(answers);  
 
         fs.writeFileSync('./README.md', content);
-        console.log('Successfully created README.md');
+        // console.log('Successfully created README.md');
     }
     ).then(() => {
         console.log('Successfully created README.md');
